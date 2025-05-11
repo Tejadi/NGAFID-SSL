94b9d5d8e2226196fdbe204f955306d22e39bc01
justinlooong91 <pak_iong_long@brown.edu>
2025-05-11 09:36:59 -0400

    set y axis value for pitch graph generation

--- utils.py
+++ utils.py
@@ -210,7 +210,7 @@ def plot_aircraft_type_comparison(original, reconstructed, aircraft_type_counts=
                     color=reconstructed_color)
             
             plt.xlabel('Time Step')
-            plt.ylabel('Value')
+            plt.ylabel('Pitch Angle (deg)')
             plt.legend()
             
             # Improve x-axis readability
